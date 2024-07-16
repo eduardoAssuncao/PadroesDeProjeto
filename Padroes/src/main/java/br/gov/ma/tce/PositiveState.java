1afeb2d2fd83c0de44036891759319c18e8e9b9c
eduardo.assuncao <eduardo.assuncao@tce.ma.gov.br>
2024-07-16 10:37:29 -0300

    feat:add-questao-2

---
+++ Padroes/src/main/java/br/gov/ma/tce/PositiveState.java
@@ -0,0 +1,25 @@
+package br.gov.ma.tce;
+
+public class PositiveState extends State{
+
+    PositiveState(Cliente cliente) {
+        super(cliente);
+        cliente.setPositive(true);
+    }
+
+    @Override
+    public Double saque(Double valor) {
+        cliente.setSaldoConta(cliente.getSaldoConta() - valor);
+        if(cliente.getSaldoConta() < 0) {
+            cliente.changeState(new NegativeState(cliente));
+        }
+        return valor;
+    }
+
+    @Override
+    public Double deposito(Double valor) {
+        valor = (valor * 0.098) * 10;
+        cliente.setSaldoConta(valor);
+        return valor;
+    }
+}
